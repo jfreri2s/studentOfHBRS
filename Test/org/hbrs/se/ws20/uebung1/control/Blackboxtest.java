ad9f9e65562a209e6eb1f7d08dbee5f06e4f6c12
jfreri2s <frericks.jan@gmx.de>
2020-11-09 12:25:16 +0100

    weitere Blackboxtests und Optimierung der Version

--- Test/org/hbrs/se/ws20/uebung1/control/Blackboxtest.java
+++ Test/org/hbrs/se/ws20/uebung1/control/Blackboxtest.java
@@ -1,6 +1,8 @@
 package org.hbrs.se.ws20.uebung1.control;
 import org.hbrs.se.ws20.uebung1.view.Client;
 import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -9,16 +11,19 @@ public class Blackboxtest{
 
     public static void main(String[] args) throws FileNotFoundException {
         Client c = new Client();
-        String erg = "Das Ergebnis der Berechnung: fünf";
-        PrintStream a = new PrintStream("test.txt");
-        System.setOut(a);
-        c.display(5);
-
+        List<Integer> testZahl = List.of(5,-5,0);
+        List<String> testString = List.of("Das Ergebnis der Berechnung: fünf",
+                                          "Das Ergebnis der Berechnung: Uebersetzung der Zahl " + -5 + " nicht möglich " + Translator.version,
+                                          "Das Ergebnis der Berechnung: Uebersetzung der Zahl " + 0 + " nicht möglich " + Translator.version );
         PrintStream originalPrintStream = new PrintStream(new FileOutputStream(FileDescriptor.out));
-        System.setOut(originalPrintStream);
-
-        Scanner sc = new Scanner(new File("test.txt"));
-        String scan = sc.nextLine();
-        System.out.println((erg.equals(scan)));
+        for(int i = 0; i <= 2; i++){
+            PrintStream a = new PrintStream("test.txt");
+            System.setOut(a);
+            c.display(testZahl.get(i));
+            System.setOut(originalPrintStream);
+            Scanner sc = new Scanner(new File("test.txt"));
+            String scan = sc.nextLine();
+            System.out.println((testString.get(i).equals(scan)));
+        }
     }
 }
