0543da0966cbcc5999ff2e22bafced9849f22d59
jfreri2s <frericks.jan@gmx.de>
2020-11-09 11:47:45 +0100

    Implementierung der display-Methode

--- src/org/hbrs/se/ws20/uebung1/view/Client.java
+++ src/org/hbrs/se/ws20/uebung1/view/Client.java
@@ -1,19 +1,23 @@
 package org.hbrs.se.ws20.uebung1.view;
 
-class Client {
+import org.hbrs.se.ws20.uebung1.control.Factory;
+import org.hbrs.se.ws20.uebung1.control.GermanTranslator;
+import org.hbrs.se.ws20.uebung1.control.Translator;
+
+
+public class Client extends GermanTranslator {
 
 	/*
 	 * Methode zur Ausgabe einer Zahl auf der Console
 	 */
-	public void display( int aNumber ){
+	public static void display( int aNumber ){
 		// In dieser Methode soll die Methode translateNumber 
 		// mit dem übergegebenen Wert der Variable aNumber 
 		// aufgerufen werden.
 		// Strenge Implementierung gegen das Interface Translator gewuenscht!
-
-		System.out.println("Das Ergebnis der Berechnung: " +
-				"[das Ergebnis an dieser Stelle]" );
-
+		Translator translator = Factory.createGermanTranslator();
+		String result = translator.translateNumber(aNumber);
+		System.out.println("Das Ergebnis der Berechnung: "+ result );
 	}
 }
 
