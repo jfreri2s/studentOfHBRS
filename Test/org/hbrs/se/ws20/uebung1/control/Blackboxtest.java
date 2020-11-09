1bbfe96a79cbffc0f4246e6e7f72139de87a182a
jfreri2s <frericks.jan@gmx.de>
2020-11-09 12:58:02 +0100

    Auslagerung auf eine Test-Methode

--- Test/org/hbrs/se/ws20/uebung1/control/Blackboxtest.java
+++ Test/org/hbrs/se/ws20/uebung1/control/Blackboxtest.java
@@ -1,16 +1,20 @@
 package org.hbrs.se.ws20.uebung1.control;
+
 import org.hbrs.se.ws20.uebung1.view.Client;
 import java.io.*;
 import java.util.List;
 import java.util.Scanner;
+
 public class Blackboxtest{
-    public static void main(String[] args) throws FileNotFoundException {
+
+    public static void test() throws FileNotFoundException {
+        PrintStream originalPrintStream = new PrintStream(new FileOutputStream(FileDescriptor.out));
         Client c = new Client();
         List<Integer> testZahl = List.of(5,-5,0);
         List<String> testString = List.of("Das Ergebnis der Berechnung: fünf",
-                                          "Das Ergebnis der Berechnung: Uebersetzung der Zahl " + -5 + " nicht möglich " + Translator.version,
-                                          "Das Ergebnis der Berechnung: Uebersetzung der Zahl " + 0 + " nicht möglich " + Translator.version );
-        PrintStream originalPrintStream = new PrintStream(new FileOutputStream(FileDescriptor.out));
+                "Das Ergebnis der Berechnung: Uebersetzung der Zahl " + -5 + " nicht möglich " + Translator.version,
+                "Das Ergebnis der Berechnung: Uebersetzung der Zahl " + 0 + " nicht möglich " + Translator.version );
+
         for(int i = 0; i <= 2; i++){
             PrintStream a = new PrintStream("test.txt");
             System.setOut(a);
@@ -20,5 +24,9 @@ public class Blackboxtest{
             String scan = sc.nextLine();
             System.out.println((testString.get(i).equals(scan)));
         }
+
+    }
+    public static void main(String[] args) throws FileNotFoundException {
+        test();
     }
 }
