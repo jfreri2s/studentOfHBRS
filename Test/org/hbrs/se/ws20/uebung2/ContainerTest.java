dcd4b67734652bdd402fe98ba915daf3507bdc88
jfreri2s <frericks.jan@gmx.de>
2020-11-24 19:40:21 +0100

    Erweiterung der Testfälle für load/save/persistent

--- Test/org/hbrs/se/ws20/uebung2/ContainerTest.java
+++ Test/org/hbrs/se/ws20/uebung2/ContainerTest.java
@@ -1,41 +1,52 @@
 package org.hbrs.se.ws20.uebung2;
 
+import org.hbrs.se.ws20.uebung3.persistence.MemberView;
+import org.hbrs.se.ws20.uebung3.persistence.PersistenceException;
+import org.hbrs.se.ws20.uebung3.persistence.PersistenceStrategyStream;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.opentest4j.AssertionFailedError;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.LinkedList;
+import java.util.List;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class ContainerTest {
     Container container;
+    MemberView mw;
     private final PrintStream standardOut = System.out;
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
     @BeforeEach
-    void setUp() {
-        container = new Container();
+    void setUp () {
+        container = Container.getInstance();
+        mw = new MemberView();
         try {
             container.addMember(new Create(1));
             container.addMember(new Create(2));
             container.addMember(new Create(3));
-
-        } catch (ContainerException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         System.setOut(new PrintStream(outputStreamCaptor));
+
     }
 
     @AfterEach
     void tearDown() {
         container = null;
+        System.setOut(standardOut);
+        PersistenceStrategyStream.datei = "Memberlist.txt";
     }
 
     @Test
     void addMemberTest() throws ContainerException {
-
         assertEquals(container.deleteMember(1),"Member (ID = 1)");
         assertEquals(container.deleteMember(2),"Member (ID = 2)");
         assertEquals(container.deleteMember(3),"Member (ID = 3)");
@@ -52,11 +63,6 @@ class ContainerTest {
         assertEquals(container.deleteMember(-5), "Die Zahl mit der ID -5 ist nicht vorhanden");
     }
 
-    @Test
-    void dumpTest() {
-        container.dump();
-        assertEquals("Member (ID = 1)\r\nMember (ID = 2)\r\nMember (ID = 3)",outputStreamCaptor.toString().trim());
-    }
 
     @Test
     void sizeTest() {
@@ -68,5 +74,60 @@ class ContainerTest {
         container.deleteMember(3);
         assertEquals(container.size(), 0);
     }
+    @Test
+    void dumpTest(){
+        mw.dump(container.getCurrentList());
+        assertEquals("Member (ID = 1)\r\nMember (ID = 2)\r\nMember (ID = 3)",outputStreamCaptor.toString().trim());
+    }
+    @Test
+    void getCurrenList(){
+        LinkedList<Member> mb;
+        mb = (LinkedList<Member>) container.getCurrentList();
+        assertEquals(mb.size(),container.size());
+    }
+    @Test
+    void persistentTest(){
+        Container secondContainer = Container.getInstance();
 
+        assertEquals(container.size(),secondContainer.size());
+        assertEquals(container.getCurrentList(),secondContainer.getCurrentList());
+        assertEquals(secondContainer.deleteMember(1), "Member (ID = 1)");
+        assertEquals(secondContainer.deleteMember(2), "Member (ID = 2)");
+        assertEquals(secondContainer.deleteMember(3), "Member (ID = 3)");
+        assertEquals(secondContainer.deleteMember(-5), "Die Zahl mit der ID -5 ist nicht vorhanden");
+
+        assertEquals(container.deleteMember(1), "Die Zahl mit der ID 1 ist nicht vorhanden");
+
+    }
+    @Test
+    void loadTestFailed() {
+        PersistenceStrategyStream.datei = null;
+        assertThrows(PersistenceException.class, () ->container.load());
+    }
+    @Test
+    void loadTestPassed(){
+        container.setpSS(new PersistenceStrategyStream<Member>());
+        try {
+            container.load();
+        } catch (PersistenceException e) {
+            e.printStackTrace();
+        }
+        mw.dump(container.getCurrentList());
+        assertEquals("Member (ID = 1)\r\nMember (ID = 2)\r\nMember (ID = 3)",outputStreamCaptor.toString().trim());
+    }
+    @Test
+    void saveTestFailed(){
+        PersistenceStrategyStream.datei = null;
+        assertThrows(PersistenceException.class, () ->container.store());
+    }
+    @Test
+    void saveTestPassed() {
+        container.setpSS(new PersistenceStrategyStream<Member>());
+        try {
+            container.store();
+        } catch (PersistenceException e) {
+            e.printStackTrace();
+        }
+        loadTestPassed();
+    }
 }
