f465a6a744a2b2909901799e7a348aca4d99035c
jfreri2s <frericks.jan@gmx.de>
2020-11-24 19:28:18 +0100

    Erweiterung der Methoden um store,load,getCurrentList und setpSS

--- src/org/hbrs/se/ws20/uebung2/Container.java
+++ src/org/hbrs/se/ws20/uebung2/Container.java
@@ -1,14 +1,26 @@
 package org.hbrs.se.ws20.uebung2;
 
 
+import org.hbrs.se.ws20.uebung3.persistence.PersistenceException;
+import org.hbrs.se.ws20.uebung3.persistence.PersistenceStrategyStream;
+
 import java.util.LinkedList;
+import java.util.List;
 
 public class Container {
 
-    LinkedList<Member> list;
-    public Container(){
+    private PersistenceStrategyStream<Member> pSS;
+    private static LinkedList<Member> list;
+    private static final Container OBJ = new Container();
+
+    private Container(){
          list = new LinkedList<>();
     }
+
+    public static Container getInstance(){
+        return OBJ;
+    }
+
     public void addMember(Member member) throws ContainerException {
         for (Member a : list) {
             if (a.getID().equals(member.getID()))
@@ -16,6 +28,7 @@ public class Container {
         }
         list.add(member);
     }
+
     public String deleteMember(Integer id){
         for (Member a : list) {
             if (a.getID().equals(id)) {
@@ -26,12 +39,29 @@ public class Container {
         }
         return "Die Zahl mit der ID " + id + " ist nicht vorhanden"; // Bei Exception kann man Stacktrace nachvollziehen. Das ist bei einem String nicht möglich
     }
-    public void dump(){
-        for (Member a: list) {
-            System.out.println(a.toString());
-        }
-    }
+
     public int size(){
         return list.size();
+    }
+
+    public void store() throws PersistenceException{
+        if(pSS == null){
+            throw new PersistenceException(PersistenceException.ExceptionType.NoStrategyIsSet,"NoStrategyIsSet");
+        }
+        pSS.save(list);
+    }
+
+    public void load() throws PersistenceException{
+        if(pSS == null){
+            throw new PersistenceException(PersistenceException.ExceptionType.NoStrategyIsSet,"NoStrategyIsSet");
+        }
+       list = (LinkedList<Member>) pSS.load();
+    }
+    public List<Member> getCurrentList(){
+        return list;
+    }
+
+    public void setpSS(PersistenceStrategyStream<Member> pSS) {
+        this.pSS = pSS;
     }
 }
