cb2667df936a120da72c00667a80117d3d3978ee
jfreri2s <frericks.jan@gmx.de>
2021-01-19 18:26:18 +0100

    Erste Implementierung ReiseAnbieterAdapter

---
+++ src/org/hbrs/se/ws20/uebung8/ReiseAnbieterAdapter.java
@@ -0,0 +1,14 @@
+package org.hbrs.se.ws20.uebung8;
+
+public class ReiseAnbieterAdapter implements HotelSucheInterface{
+    private ReiseAnbieter reiseAnb;
+    public SuchErgebnis suche(SuchAuftrag s){
+        return null;
+    }
+    public QueryObject transformInput(SuchAuftrag s){
+        return null;
+    }
+    public SuchErgebnis transformOutput(QueryResult q){
+        return null;
+    }
+}
