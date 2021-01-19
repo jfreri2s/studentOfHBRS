f17b9a4ffc475af7cf5efbfeee4b5bfdfb27e1b6
jfreri2s <frericks.jan@gmx.de>
2021-01-19 18:24:11 +0100

    Factory Klasse fürs Singleton-Pattern

---
+++ src/org/hbrs/se/ws20/uebung8/Factory.java
@@ -0,0 +1,7 @@
+package org.hbrs.se.ws20.uebung8;
+
+public class Factory {
+    public static ReiseAnbieterAdapter createReiseAnbieterAdapter(){
+        return new ReiseAnbieterAdapter();
+    }
+}
